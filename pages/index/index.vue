03093b1288482e392a8a9b8c63fbc8fb756c83ba
pyl0603 <742531312@qq.com>
2021-11-23 11:36:29 +0800

    2021-11-23-1-uniapp learn

---
+++ pages/index/index.vue
@@ -0,0 +1,24 @@
+<template>
+	<view>
+		首页
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+</style>
